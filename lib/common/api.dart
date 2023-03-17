8397ca3e4daf3fb68bd0c61b87cadc53ad214880
AR Rahman <arrahman.bd@outlook.com>
2023-03-17 20:57:36 +0600

    Internet issue fixed
    Fixed no internet issue on release

--- lib/common/api.dart
+++ lib/common/api.dart
@@ -1,3 +1,3 @@
 const baseUrl = "https://api.openweathermap.org/data/";
 const version = '2.5/';
-const apikey = '4b74f67cf0b07d01b38fa272f953b290';
+const apikey = '4f7069832b589ff242e1e485eb55840b';
